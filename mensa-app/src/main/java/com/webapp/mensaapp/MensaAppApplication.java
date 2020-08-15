570f32a56a5ca6597d7c566d56e9d6153b8c3622
Matien <ms@elbdev.de>
2020-08-15 11:56:37 +0200

    project istallation

---
+++ mensa-app/src/main/java/com/webapp/mensaapp/MensaAppApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.mensaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensaAppApplication.class, args);
+	}
+
+}
