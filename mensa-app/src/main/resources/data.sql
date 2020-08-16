3d93e1d5afb00d55a66ab4b332c79dfd7a1bdd79
Matien <ms@elbdev.de>
2020-08-16 12:12:06 +0200

    database

--- mensa-app/src/main/resources/data.sql
+++ mensa-app/src/main/resources/data.sql
@@ -1,8 +1,24 @@
 DROP TABLE IF EXISTS billionaires;
 
-CREATE TABLE billionaires (
+CREATE TABLE essen (
+  id_essen INT AUTO_INCREMENT  PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  art VARCHAR(20) NOT NULL,
+  price int NOT NULL
+);
+
+CREATE TABLE essensplan (
   id INT AUTO_INCREMENT  PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL,
-  career VARCHAR(250) DEFAULT NULL
+  wochentag VARCHAR(50) NOT NULL,
+  essen VARCHAR(20) NOT NULL,
+  art VARCHAR(20) NOT NULL,
+  price int NOT NULL
+);
+
+CREATE TABLE essensplan_pro_woche (
+  id_woche INT AUTO_INCREMENT  PRIMARY KEY,
+  wochentag VARCHAR(50) NOT NULL,
+  essen VARCHAR(20) NOT NULL,
+  art VARCHAR(20) NOT NULL,
+  price int NOT NULL
 );
