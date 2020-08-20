8d56f515f1ffb0a9ea2633428f87921ae7f3bc67
Matien <ms@elbdev.de>
2020-08-20 12:08:52 +0200

    foodweek table for a week database

--- mensa-app/src/main/java/com/webapp/mensaapp/model/Weekday.java
+++ mensa-app/src/main/java/com/webapp/mensaapp/model/Weekday.java
@@ -1,9 +1,36 @@
 package com.webapp.mensaapp.model;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Weekday {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday
+    Monday(1),
+    Tuesday(2),
+    Wednesday(3),
+    Thursday(4),
+    Friday(5);
+
+    private int i;
+    private static Map map = new HashMap<>();
+
+    Weekday(int i) {
+        this.i = i;
+    }
+
+    static {
+        for (Weekday weekday : Weekday.values()) {
+            map.put(weekday.i, weekday);
+        }
+    }
+
+    public static Weekday valueOf(int weekday) {
+        return (Weekday) map.get(weekday);
+    }
+
+    public int getValue() {
+        return i;
+    }
+
+
 }
--- mensa-app/src/main/resources/data.sql
+++ mensa-app/src/main/resources/data.sql
@@ -1,3 +1,19 @@
+INSERT INTO meal (name, type, price)
+VALUES ('Vollkornnudeln', 'vegan', 2.30),
+('Falafel', 'vegan', 1.90),
+('Reishänchenpfanne', 'fleischhaltig', 2.60),
+('Burger', 'fleischhaltig', 2.60),
+('Kartoffelpüree', 'vegetarisch', 2.30),
+('Fischfilet', 'vegetarisch', 2.90);
 
 INSERT INTO meal_table (calendar_week)
 VALUES (1),(2),(3),(4),(5),(6),(7);
+
+INSERT INTO meal_table_meal_table_week (meal_table_id, meal_table_week_id, meal_table_week_key)
+VALUES
+(1, 4, 1),
+(1, 3, 2),
+(1, 1, 3),
+(1, 5, 4),
+(1, 2, 5);
+
